f8a913debdcc8f43e5e57be2c5c0d682b8377448
kingl4166 <36107426+kingl4166@users.noreply.github.com>
2018-02-03 10:34:38 -0500

    Add files via upload

---
+++ P2T1_SalesPrediction_KingLafayette.py
@@ -0,0 +1,11 @@
+# CTI-110
+# P2T1 - Sales Prediction
+# Lafayette King
+# 2/1/2018
+
+
+
+
+total_sales = float(input('Enter the projected sales: '))
+profit = total_sales * 0.23
+print('The profit is $' , format (profit, ',.2f')) 
