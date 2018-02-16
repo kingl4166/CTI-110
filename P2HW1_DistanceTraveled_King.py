e39e07d18e6fc4752a31f945364c682d4ae83d60
kingl4166 <36107426+kingl4166@users.noreply.github.com>
2018-02-16 14:32:35 -0500

    Add files via upload

---
+++ P2HW1_DistanceTraveled_King.py
@@ -0,0 +1,27 @@
+# CTI- 110
+# P2HW1 - Distance Traveled
+# Lafayette King
+# 2/16/2018
+# speed = 70
+
+
+
+
+
+
+
+speed =70
+time = 6       
+distance = speed * time
+print("The distance travelled is", distance)
+
+speed =70                                       
+time = 10
+distance = speed * time
+print("The distance travelled is", distance)
+
+
+speed =70
+time = 15
+distance = speed * time
+print("The distance travelled is", distance)
