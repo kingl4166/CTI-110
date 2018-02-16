379cc535c818546eca53339bcbf224cd68e813e8
kingl4166 <36107426+kingl4166@users.noreply.github.com>
2018-02-16 14:31:45 -0500

    Delete P2HW1_DistanceTraveled_King.py

--- P2HW1_DistanceTraveled_King.py
+++
@@ -1,9 +0,0 @@
-# CTI-110
-# P2HW1- Distance Traveled
-# Lafayette King
-# 2/2/2018
-#
-print = (input('speed is 70 mph')) 
-print = (input('distanceafter6 is 420 miles'))
-print = (input('distanceafter10 is 700 miles'))
-print = (input('distanceafter15 is 1050 miles')) 
