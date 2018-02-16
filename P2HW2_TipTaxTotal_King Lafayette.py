b7da5722f02656d6d6a365847f51d0db9af83712
kingl4166 <36107426+kingl4166@users.noreply.github.com>
2018-02-16 14:40:02 -0500

    Add files via upload

---
+++ P2HW2_TipTaxTotal_King Lafayette.py
@@ -0,0 +1,18 @@
+3# CTI-110 
+# P2HW2 - Tip Tax Total
+# Lafayette King
+# 2/3/2018
+# TipAmount = 0.18
+# salesTax = 0.07 
+foodcost = float(input('Enter the foodcost'))
+totalcost = foodcost * 0.07
+print('The salestax is  $', format (totalcost ,'.2f'))
+print('totalcost:', foodcost + totalcost )  
+print('TipAmount:', foodcost  *  0.18)    
+TipAmount = float(input('TipAmount'))     
+print('FinalAmount:', totalcost + TipAmount + foodcost)                           
+  
+
+ 
+
+
