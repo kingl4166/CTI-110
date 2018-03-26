c4bc292cd10a5c372e14388d159282d9a7ff7433
kingl4166 <36107426+kingl4166@users.noreply.github.com>
2018-03-25 22:20:19 -0400

    Add files via upload

---
+++ P4LAB1_King.py.py
@@ -0,0 +1,76 @@
+
+# CTI-110
+# P4T1a: Shapes
+# Lafayette King
+# 3/1/2018
+
+
+
+# start
+import turtle
+win = turtle.Screen()
+ben = turtle.Turtle()
+ben.shape("turtle") 
+
+for i in (0,1,2,3):
+ 
+  ben.forward(50)
+  ben.left(90)
+  ben.forward(50)
+  ben.left(90)
+  ben.forward(50)
+  ben.left(90)
+  ben.forward(50)
+  ben.left(90)
+
+
+  ben.forward(100)
+  ben.left(90)
+  ben.forward(50)
+  ben.left(90)
+  ben.forward(100)
+  ben.left(90)
+  ben.forward(50)
+  ben.left(90)
+
+
+win.mainloop() 
+
+
+
+
+
+
+
+
+
+
+# start
+import turtle
+win = turtle.Screen()
+ben = turtle.Turtle()
+ben.shape("turtle") 
+
+for i in (0,1,2,3):
+ 
+  ben.forward(50)
+  ben.left(90)
+  ben.forward(50)
+  ben.left(90)
+  ben.forward(50)
+  ben.left(90)
+  ben.forward(50)
+  ben.left(90)
+
+
+  ben.forward(100)
+  ben.left(90)
+  ben.forward(50)
+  ben.left(90)
+  ben.forward(100)
+  ben.left(90)
+  ben.forward(50)
+  ben.left(90)
+
+
+win.mainloop() 
