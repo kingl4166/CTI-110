390034f4cb9b17969e6e963d733ff3d97414c447
kingl4166 <36107426+kingl4166@users.noreply.github.com>
2018-04-10 12:37:19 -0400

    Add files via upload

---
+++ P4HW3_Factorial_King.py
@@ -0,0 +1,18 @@
+# CTI-110
+#P4HW3: Factorial
+#Lafayette King
+# 3/12/2018
+
+num = 1
+
+n = int(input("Enter a nonnegative integer? ")) 
+          
+for i in range(n,0,-1):
+        
+    num = num * i
+        
+print("The Factorial of", n , "is", num) 
+
+
+
+
