a50621acb6d7ba09f8582449770636f273494f2a
kingl4166 <36107426+kingl4166@users.noreply.github.com>
2018-04-10 12:34:40 -0400

    Add files via upload

---
+++ P4T2_King.py
@@ -0,0 +1,25 @@
+# CTI-110
+# P4T2_Bug Collector
+# Lafayette King
+# 3/5/2018
+
+
+
+
+# start    
+GrandTotal = 0
+for day in range(1,8):
+  print("Enter the bugs collected on day",day)
+  bugs = int(input())
+  GrandTotal += bugs
+  
+  
+  
+  print("you collected a GrandTotal of", GrandTotal, "bugs." ) 
+    
+
+  
+ 
+
+
+
