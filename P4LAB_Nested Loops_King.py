a067474ed8571b33db091054887f03f1be683021
kingl4166 <36107426+kingl4166@users.noreply.github.com>
2018-04-10 12:38:13 -0400

    Add files via upload

---
+++ P4LAB_Nested Loops_King.py
@@ -0,0 +1,73 @@
+
+
+
+
+
+
+
+#initialize
+import turtle
+wn = turtle.Screen()
+
+# make your turtle
+ben = turtle.Turtle()
+ben.shape("turtle")
+ben.color("blue")
+ben.pensize(5)
+ben . speed(3)
+ben . left(90)
+for i in range(5):
+     
+    ben . forward(100)
+    ben . forward(-80)
+ 
+    ben . left(45)
+    ben . forward(25)
+   
+    ben . forward(-25)
+    ben . right(90) 
+    ben . forward(25)
+    ben . forward(-25)
+    ben . left(45)
+    ben . forward(20)
+    ben . left(45)
+    ben . forward(25)
+   
+    ben . forward(-25)
+    ben . right(90) 
+    ben . forward(25)
+    ben . forward(-25)
+    ben . left(45)
+    ben . forward(20)
+     
+    ben . left(45)
+    ben . forward(25)
+   
+    ben . forward(-25)
+    ben . right(90) 
+    ben . forward(25)
+    ben . forward(-25)
+    ben . left(45)
+    ben . forward(20)
+    ben . left(45)
+    ben . forward(25)
+   
+    ben . forward(-25)
+    ben . right(90) 
+    ben . forward(25)
+    ben . forward(-25)
+    ben . left(45)
+    ben . forward(20)
+     
+    ben. forward(-100)
+    ben . left(132)
+    ben . forward(25)
+
+
+
+    ben . right(60)
+     
+    
+    
+
+    
