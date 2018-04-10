5783ae008836a35cea7addd387227bf58094db2a
kingl4166 <36107426+kingl4166@users.noreply.github.com>
2018-04-10 12:33:13 -0400

    Add files via upload

---
+++ P4LAB2_King.py.py
@@ -0,0 +1,39 @@
+# CTI-110
+# P4T1b_Intials
+# Lafayette King
+# 3/2/2018 
+
+
+
+
+
+
+
+#initialize
+import turtle
+wn = turtle.Screen()
+
+# make your turtle
+ben = turtle.Turtle()
+ben.shape("turtle")
+ben.color("red")
+ben.pensize(10)
+ben.speed(1)
+
+ben . right(90)
+ben . forward(100)
+ben . left(90)
+ben . forward(50)
+ben . penup()
+ben . forward(100)
+ben . pendown()
+ben . left(90)
+ben . forward(100)
+ben . back(50)
+ben . right(45)
+ben . forward(60)
+ben . back(60)
+ben . right(90)
+ben . forward(60)
+# at the end, keep the window open until closed
+wn.mainloop()
