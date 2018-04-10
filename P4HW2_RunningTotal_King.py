12a4cbf1c6eab6063c923608078c21d175558413
kingl4166 <36107426+kingl4166@users.noreply.github.com>
2018-04-10 12:35:59 -0400

    Add files via upload

---
+++ P4HW2_RunningTotal_King.py
@@ -0,0 +1,27 @@
+
+    
+total = -1
+x = 1
+while x >= 1 :
+  number = input("enter a number: ")
+  total = total + x
+  x = float(number)
+print("total:",total)   
+
+
+
+
+    
+  
+ 
+  
+    
+  
+   
+    
+   
+    
+
+
+    
+ 
