656325e9c64393ffb9a93a5041dd972879036d81
kingl4166 <36107426+kingl4166@users.noreply.github.com>
2018-04-23 15:50:27 -0400

    Add files via upload

---
+++ P4HW1_DistanceTraveled_King.py
@@ -0,0 +1,42 @@
+# CTI-110
+# P4HW1: Distance Traveled
+# Lafayette King
+# 03/10/2018 
+
+def main():
+    distanceTraveled = speed * hours
+    
+   
+
+     
+print(int(input("what is the speed of the vehicle in mph?"))) 
+
+print(int(input("How many hours has it traveled?")))
+speed = 40
+
+
+for hours in (1,2,3,4,5):
+    distanceTraveled = speed * hours
+    print("hours", hours , "\t", "distanceTravelled", distanceTraveled)
+
+
+    main() 
+    
+  
+    
+    
+        
+    
+    
+
+
+     
+    
+       
+        
+                       
+
+
+
+
+
